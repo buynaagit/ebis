47dc999cbb7a7338102bff6cfd44dcb064d66f09
buynaa <6uynaa@gmail.com>
2022-05-16 14:32:20 +0800

    videoplayer

--- src/components/Banner/EventBanner.js
+++ src/components/Banner/EventBanner.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { getLocale } from "../../locales";
 import YoutubeEmbed from "../YoutubeEmbed";
 
-
-
-
 const EventBanner = () => {
+  const [videoPlayer, setVideoPlayer] = useState(false);
+
+  const toggleVideoPlayer = () => {
+    setVideoPlayer(!videoPlayer);
+  };
+
   return (
     <section className="event_banner_area" id="hospitalBanner">
-   
       <div
         className="parallax-effect"
         style={{
@@ -20,55 +22,61 @@ const EventBanner = () => {
       <div className="container">
         <div className="event_banner_content">
           <div className="round wow zoomIn" data-wow-delay="0.2s"></div>
-          <div className="event_about_item wow fadeInUp" data-wow-delay="0.2s">
-            <img
-              style={{ width: 100, height: 100 }}
-              src={require("../../img/home-event/mongol.png")}
-              alt=""
-            />
-            <img
-              style={{
-                width: 100,
-                height: 100,
-                marginLeft: 20,
-                borderRadius: 20,
-              }}
-              src={require("../../img/home-event/japan.png")}
-              alt=""
-            />
-          </div>
-          <h2
-            style={{ textTransform: "uppercase" }}
-            className="wow fadeInUp"
-            data-wow-delay="0.8s"
-          >
-            <span> {getLocale("title")} </span>
-            <br />
-            {getLocale("orthopedic_c")}
-          </h2>
+          {videoPlayer === false && (
+            <div
+              className="event_about_item wow fadeInUp"
+              data-wow-delay="0.2s"
+            >
+              <div>
+                <img
+                  style={{ width: 100, height: 100 }}
+                  src={require("../../img/home-event/mongol.png")}
+                  alt=""
+                />
+                <img
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginLeft: 20,
+                    borderRadius: 20,
+                  }}
+                  src={require("../../img/home-event/japan.png")}
+                  alt=""
+                />
+              </div>
+              <h2
+                style={{ textTransform: "uppercase" }}
+                className="wow fadeInUp"
+                data-wow-delay="0.8s"
+              >
+                <span> {getLocale("title")} </span>
+                <br />
+                {getLocale("orthopedic_c")}
+              </h2>
+              <a
+                className="event_btn btn_hover wow fadeInLeft"
+                data-wow-delay="0.9s"
+                href="https://www.facebook.com/%D0%AD%D0%91%D0%98%D0%A1-%D0%A1%D1%8D%D1%80%D0%B3%D1%8D%D1%8D%D0%BD-%D0%B7%D0%B0%D1%81%D0%B0%D1%85-%D0%BA%D0%BB%D0%B8%D0%BD%D0%B8%D0%BA-%D0%AF%D0%BF%D0%BE%D0%BD-%D0%B1%D0%B0%D1%80%D0%B8%D0%B0-%D0%B7%D0%B0%D1%81%D0%B0%D0%BB-%D0%A3%D1%82%D0%B0%D1%81-9450-0054-8006-1415-104619461022471/"
+              >
+                Facebook
+              </a>
+            </div>
+          )}
           <a
-            className="event_btn btn_hover wow fadeInLeft"
-            data-wow-delay="0.9s"
-            href="https://www.facebook.com/%D0%AD%D0%91%D0%98%D0%A1-%D0%A1%D1%8D%D1%80%D0%B3%D1%8D%D1%8D%D0%BD-%D0%B7%D0%B0%D1%81%D0%B0%D1%85-%D0%BA%D0%BB%D0%B8%D0%BD%D0%B8%D0%BA-%D0%AF%D0%BF%D0%BE%D0%BD-%D0%B1%D0%B0%D1%80%D0%B8%D0%B0-%D0%B7%D0%B0%D1%81%D0%B0%D0%BB-%D0%A3%D1%82%D0%B0%D1%81-9450-0054-8006-1415-104619461022471/"
-          >
-            Facebook
-          </a>
-          <a
+            onClick={toggleVideoPlayer}
             className="event_btn event_btn_two btn_hover wow fadeInRight"
             data-wow-delay="0.9s"
-            href="https://www.facebook.com/104619461022471/videos/1050756605264980/"
-            // style={{ color: "black" }}
+            // href="https://www.facebook.com/104619461022471/videos/1050756605264980/"
+            style={{ color: "#fba820" }}
           >
             <i className="arrow_triangle-right_alt2"></i>
             {getLocale("intro_vid")}
           </a>
+          {videoPlayer === true && <YoutubeEmbed embedId="rokGy0huYEA" />}
         </div>
-        <h1>Youtube Embed</h1>
-      <YoutubeEmbed embedId="rokGy0huYEA" />
       </div>
     </section>
   );
 };
-
 
 export default EventBanner;
